ffa2c27ad7a52cecd361ace66a73842c326598e9
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2024-11-07 15:21:43 +0300

    #####

--- help-site/src/app/page.jsx
+++ help-site/src/app/page.jsx
@@ -1,8 +1,17 @@
 function Home() {
   return (
-    <div>
-      <h1>wel-come</h1>
-    </div>
+    <section className="w-full flex-center flex-col">
+    <h1 className="head_text text-center">
+        Discover & share
+    <br  className="max-md:hidden"/>
+    <span className=" orange_gradient text-center">
+        AI-Powered Prompts
+    </span></h1>
+    <p className="desc text-center">
+        promptopia is an open-source AI promting tool for moder world to discover,
+        create and share creative prompts
+    </p>
+    </section>
   )
 }
 export default Home
