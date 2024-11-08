58b5ea21ae7af32d18639057b9b665c2934c2b51
Hakimii12 <117950633+Hakimii12@users.noreply.github.com>
2024-11-08 23:26:16 +0300

    #####

--- help-site/components/Nav.jsx
+++ help-site/components/Nav.jsx
@@ -3,16 +3,32 @@ import Image from "next/image"
 import Link from "next/link"
 import { signIn,signOut,useSession,getProviders} from 'next-auth/react'
 function Nav() {
+    const isUserLoggedIn=true;
   return (
-    <nav className="w-full flex-start ">
-     <Image
+    <nav className="w-full flex-between ">
+        <Link href="/" className="flex gap-2 flex-center">
+         <Image
      src="/assets/images/logo.svg"
      width={30}
      height={30}
      alt="promtopia logo"
      className="object-contain"
      />
-     <p className="logo_text">Promptopia</p>
+      <p className="logo_text">Promptopia</p>
+        </Link>
+   <div className="btn_display "> 
+        {isUserLoggedIn ? 
+        <div className=" flex gap-2 md:gap-5  btn_aligne">
+            <Link href="/components" className="black_btn ">
+                    create post
+            </Link>
+            <Link href='/signout' className="outline_btn ">
+                 signOut
+            </Link>
+        </div>
+        
+    :"dsl"}</div>
+    
     </nav>
   )
 }
